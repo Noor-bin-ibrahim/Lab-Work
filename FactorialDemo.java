4e72711cbfbb3d0ff610649af8faf90d44b60fc3
Noor-bin-ibrahim <84558518+Noor-bin-ibrahim@users.noreply.github.com>
2021-06-06 15:28:25 +0530

    Add files via upload

---
+++ FactorialDemo.java
@@ -0,0 +1,30 @@
+package lab;
+
+import java.util.Scanner;
+
+public class FactorialDemo {
+
+	public static void main(String[] args) {
+		// Scanner Object for capturing the user input
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Enter the Number:");
+		//Stored the entered value in variable
+		int num = scanner.nextInt();
+		//Called the user defined function fact
+		int factorial = fact(num);
+		System.out.println("Factorial of entered number is: "+factorial);
+		}
+		static int fact(int n)
+		{
+		int output;
+		if(n==1){
+		return 1;
+		}
+		//Recursion: Function calling itself!!
+		output = fact(n-1)* n;
+		return output;
+		
+
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package lab;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("I Love to work in JAVA");
+
+	}
+
+}
